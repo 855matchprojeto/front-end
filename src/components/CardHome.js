240282f07df49ba1957e48b53eb2b46b4b9f9732
caiolsds <caiolsds@gmail.com>
2021-09-06 22:57:29 -0300

    Coloquei imagem nos cards

--- src/components/CardHome.js
+++ src/components/CardHome.js
@@ -6,19 +6,33 @@ import {
   CardContent,
   CardActions,
   Button,
+  makeStyles,
 } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  media: {
+    height: 0,
+    paddingTop: "56.25%",
+  },
+  actions: {
+    display: "flex",
+    justifyContent: "space-around",
+  },
+}));
+
 const CardHome = ({ info }) => {
+  const classes = useStyles();
   return (
     <Grid item xs={12} md={4} lg={3}>
       <Card>
-        <CardMedia image={info.image} />
+        <CardMedia className={classes.media} image={info.image} />
         <CardContent>
           <Typography variant="subtite1">{info.title}</Typography>
           <p>{info.description}</p>
         </CardContent>
-        <CardActions>
+        <CardActions className={classes.actions}>
           <Button color="primary">Tenho interesse</Button>
+          <Button color="secondary">Saiba mais</Button>
         </CardActions>
       </Card>
     </Grid>
