258a4b0f4e6ed0d7cfabfb2a0836380f60482a6a
caiolsds <caiolsds@gmail.com>
2021-09-06 23:00:01 -0300

    Coloquei imagem nos cards

--- src/components/CardHome.js
+++ src/components/CardHome.js
@@ -23,7 +23,7 @@ const useStyles = makeStyles((theme) => ({
 const CardHome = ({ info }) => {
   const classes = useStyles();
   return (
-    <Grid item xs={12} md={4} lg={3}>
+    <Grid item xs={12} sm={6} md={4} lg={3}>
       <Card>
         <CardMedia className={classes.media} image={info.image} />
         <CardContent>
