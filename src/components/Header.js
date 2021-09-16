60dad48755238ed08f156069665a7a81d1d77c34
caiolsds <caiolsds@gmail.com>
2021-09-15 21:31:50 -0300

    Adicionei uma barra de menu para mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  makeStyles,
+  IconButton,
+  Drawer,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
@@ -11,6 +19,7 @@ const useStyles = makeStyles((theme) => ({
 
   brand: {
     flexGrow: 1,
+    marginLeft: theme.spacing(3),
   },
   nav: {
     flexGrow: 3,
@@ -20,39 +29,128 @@ const useStyles = makeStyles((theme) => ({
   navLink: {
     margin: theme.spacing(1, 5),
     textDecoration: "none",
-    color: "white",
+    color: "inherit",
     fontSize: "1rem",
+  },
+  "@media (max-width: 900px)": {
+    paddingLeft: 0,
+  },
+  drawer: {
+    backgroundColor: theme.palette.primary.main,
+    width: "50%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  navLinkMobile: {
+    margin: theme.spacing(1, 5),
+    textDecoration: "none",
+    color: "white",
+    fontSize: "1.2rem",
+    padding: theme.spacing(3),
+    fontWeight: "bold",
   },
 }));
 
 const Header = () => {
   const classes = useStyles();
+  const [view, setView] = useState({ mobileView: false, drawerOpen: false });
+
+  useEffect(() => {
+    const setResponsiveView = () => {
+      if (window.innerWidth < 900) {
+        setView((previous) => ({ ...previous, mobileView: true }));
+      } else {
+        setView((previous) => ({ ...previous, mobileView: false }));
+      }
+    };
+
+    setResponsiveView();
+    window.addEventListener("resize", () => setResponsiveView());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveView());
+    };
+  }, []);
+
+  const DisplayDesktop = () => {
+    return (
+      <Toolbar className={classes.toolbar}>
+        <Typography className={classes.brand} variant="h5">
+          Match de Projetos
+        </Typography>
+        <nav className={classes.nav}>
+          <Link to="/home" className={classes.navLink}>
+            Home
+          </Link>
+          <Link to="/" className={classes.navLink}>
+            Projetos
+          </Link>
+          <Link to="/" className={classes.navLink}>
+            Interesses
+          </Link>
+          <Link to="/perfil" className={classes.navLink}>
+            Perfil
+          </Link>
+          <Link to="/" className={classes.navLink}>
+            Sair
+          </Link>
+        </nav>
+      </Toolbar>
+    );
+  };
+
+  const DisplayMobile = () => {
+    const handleDrawerOpen = () => {
+      setView((previous) => ({ ...previous, drawerOpen: true }));
+    };
+    const handleDrawerClose = () => {
+      setView((previous) => ({ ...previous, drawerOpen: false }));
+    };
+    return (
+      <Toolbar>
+        <IconButton
+          {...{ color: "inherit", edge: "start", onClick: handleDrawerOpen }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          {...{
+            anchor: "left",
+            open: view.drawerOpen,
+            onClose: handleDrawerClose,
+            classes: {
+              paper: classes.drawer,
+            },
+          }}
+        >
+          <Link to="/home" className={classes.navLinkMobile}>
+            Home
+          </Link>
+          <Link to="/" className={classes.navLinkMobile}>
+            Projetos
+          </Link>
+          <Link to="/" className={classes.navLinkMobile}>
+            Interesses
+          </Link>
+          <Link to="/perfil" className={classes.navLinkMobile}>
+            Perfil
+          </Link>
+          <Link to="/" className={classes.navLinkMobile}>
+            Sair
+          </Link>
+        </Drawer>
+        <Typography className={classes.brand} variant="h5">
+          Match de Projetos
+        </Typography>
+      </Toolbar>
+    );
+  };
 
   return (
     <header>
       <AppBar position="static">
-        <Toolbar className={classes.toolbar}>
-          <Typography className={classes.brand} variant="h5">
-            Match de Projetos
-          </Typography>
-          <nav className={classes.nav}>
-            <Link to="/home" className={classes.navLink}>
-              Home
-            </Link>
-            <Link to="/" className={classes.navLink}>
-              Projetos
-            </Link>
-            <Link to="/" className={classes.navLink}>
-              Interesses
-            </Link>
-            <Link to="/perfil" className={classes.navLink}>
-              Perfil
-            </Link>
-            <Link to="/" className={classes.navLink}>
-              Sair
-            </Link>
-          </nav>
-        </Toolbar>
+        {view.mobileView ? <DisplayMobile /> : <DisplayDesktop />}
       </AppBar>
     </header>
   );
--- src/components/Perfil.js
+++ src/components/Perfil.js
@@ -1,9 +1,31 @@
 import React from "react";
-import { Container, Typography, makeStyles } from "@material-ui/core";
+import {
+  Container,
+  Typography,
+  makeStyles,
+  TextField,
+} from "@material-ui/core";
 import Header from "./Header";
 
 const useStyles = makeStyles((theme) => ({
-  container: { marginTop: theme.spacing(1) },
+  container: {
+    marginTop: theme.spacing(1),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "50%",
+    display: "flex",
+    flexDirection: "column",
+    marginTop: theme.spacing(5),
+  },
+  textField: {
+    marginBottom: theme.spacing(3),
+  },
+  title: {
+    marginTop: theme.spacing(4),
+  },
 }));
 const Perfil = () => {
   const classes = useStyles();
@@ -11,7 +33,33 @@ const Perfil = () => {
     <>
       <Header />
       <Container className={classes.container}>
-        <Typography variant="h5">Perfil</Typography>
+        <Typography className={classes.title} variant="h4">
+          Perfil
+        </Typography>
+        <form className={classes.form}>
+          <TextField
+            className={classes.textField}
+            type="input"
+            label="Email"
+            value=""
+            placeholder="Email"
+            disabled
+          />
+          <TextField
+            className={classes.textField}
+            type="input"
+            label="Name"
+            value=""
+            placeholder="Name"
+          />
+          <TextField
+            className={classes.textField}
+            type="input"
+            label="Sobrenome"
+            value=""
+            placeholder="Sobrenome"
+          />
+        </form>
       </Container>
     </>
   );
