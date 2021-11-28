b3e6ab6060762c9ba85f973a241b0c017dca6cd7
caiolsds <caiolsds@gmail.com>
2021-11-28 13:47:05 -0300

    Foram adicionadas algumas requisições em projetos

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   Container,
   Grid,
@@ -12,6 +12,7 @@ import {
   Card,
 } from "@mui/material";
 import UploadIcon from "@mui/icons-material/Upload";
+import { getToken } from "../services/auth";
 
 import axios from "axios";
 
@@ -21,6 +22,8 @@ const Projetos = () => {
     titulo: "",
     descricao: "",
   });
+  const [allInteresses, setAllInteresses] = useState(null);
+  const [allCourses, setAllCourses] = useState(null);
   const defaultImageUrl =
     "https://rockcontent.com/br/wp-content/uploads/sites/2/2020/04/modelo-de-projeto.png";
 
@@ -29,27 +32,36 @@ const Projetos = () => {
   ]);
 
   const [imageFile, setImageFile] = useState(null);
+  const getInteresses = async () => {
+    const URL = "https://perfis-match-projetos.herokuapp.com/interests";
+    try {
+      const res = await axios.get(URL, {
+        headers: {
+          Authorization: "Bearer " + getToken,
+          Accept: "application/json",
+        },
+      });
+      setAllInteresses(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  const cursos = [
-    { label: "Administração" },
-    { label: "Matemática" },
-    { label: "Engenharia de Computação" },
-    { label: "Medicina" },
-    { label: "Enfermagem" },
-    { label: "Ciencia da Computação" },
-    { label: "Física" },
-    { label: "Engenharia Elétrica" },
-  ];
-
-  const areas = [
-    { label: "Cálculo" },
-    { label: "Mecânica" },
-    { label: "Machine Learning" },
-    { label: "Web Development" },
-    { label: "Resistência dos Materiais" },
-    { label: "Álgebra Linear" },
-    { label: "Física Quântica" },
-  ];
+  const getCursos = async () => {
+    const URL = "https://perfis-match-projetos.herokuapp.com/courses";
+    try {
+      const res = await axios.get(URL, {
+        headers: {
+          Authorization: "Bearer " + getToken,
+          Accept: "application/json",
+        },
+      });
+      setAllCourses(res.data);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const [isLoading, setIsLoading] = useState(false);
   const [areasSelecionadas, setAreasSelecionadas] = useState([]);
@@ -70,31 +82,24 @@ const Projetos = () => {
   const handleCreateProject = async (e) => {
     // Faz as requisições para adicionar o projeto, e desativa o botao enquanto faz a requisição
     setIsLoading(true);
+    const form = {
+      titulo: fields.titulo,
+      descricao: fields.descricao,
+      interesses: areasSelecionadas.map((area) => area.id),
+      cursos: cursosSelecionados.map((curso) => curso.id),
+      autores: [],
+    };
 
-    // const form = new FormData();
-    // const info = {
-    //   ...fields,
-    //   areas: areasSelecionadas,
-    //   cursos: cursosSelecionados,
-    //   imageFile: imageFile,
-    // };
-
-    // form.append("titulo", fields.titulo);
-    // form.append("descricao", fields.descricao);
-    // form.append("areas", areasSelecionadas);
-    // form.append("cursos", cursos);
-    // form.append("image", imageFile, imageFile.name);
-
-    // try {
-    //   const res = await axios.post("ENDPOINT", form, {
-    //     headers: { "Content-Type": "multipart/form-data" },
-    //   });
-    //   console.log(res);
-    // } catch (err) {
-    //   console.log(err);
-    // }
-
-    console.log("Sucesso?");
+    try {
+      const res = await axios.post(
+        "https://projetos-match-projetos.herokuapp.com/projetos",
+        form,
+        { headers: { "Content-Type": "application/json" } }
+      );
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
 
     setIsLoading(false);
   };
@@ -105,6 +110,11 @@ const Projetos = () => {
       setImageFile(e.target.files[0]);
     }
   };
+
+  useEffect(() => {
+    getInteresses();
+    getCursos();
+  }, []);
 
   return (
     <Container maxWidth="xl" sx={{ mb: 5 }}>
@@ -168,13 +178,14 @@ const Projetos = () => {
                 <Stack spacing={3} sx={{ width: "100%" }}>
                   <Autocomplete
                     multiple
-                    options={cursos.map((curso) => curso.label)}
+                    options={allCourses && allCourses}
+                    getOptionLabel={(option) => option.nome_exibicao}
                     freeSolo
                     renderTags={(value, getTagProps) =>
                       value.map((option, index) => (
                         <Chip
                           variant="outlined"
-                          label={option}
+                          label={option.nome_exibicao}
                           {...getTagProps({ index })}
                         />
                       ))
@@ -195,7 +206,8 @@ const Projetos = () => {
                 <Stack spacing={3} sx={{ width: "100%" }}>
                   <Autocomplete
                     multiple
-                    options={areas.map((area) => area.label)}
+                    options={allInteresses && allInteresses}
+                    getOptionLabel={(option) => option.nome_exibicao}
                     name="areas"
                     id="areas"
                     freeSolo
@@ -203,12 +215,15 @@ const Projetos = () => {
                       value.map((option, index) => (
                         <Chip
                           variant="outlined"
-                          label={option}
+                          label={option.nome_exibicao}
                           {...getTagProps({ index })}
                         />
                       ))
                     }
-                    onChange={(e, value) => handleChangeAreas(e, value)}
+                    onChange={(e, value) => {
+                      handleChangeAreas(e, value);
+                      console.log(value);
+                    }}
                     renderInput={(params) => (
                       <TextField
                         {...params}
