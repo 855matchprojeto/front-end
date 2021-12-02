a67d40e9d99c6f874c882c36d8e27e42b46866b2
Desnord <desnord12@gmail.com>
2021-12-02 14:55:08 -0300

    card styles+

--- src/components/ProfCard.js
+++ src/components/ProfCard.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Card, Grid, CardMedia, Typography,Box } from "@mui/material";
-import { CardContent, CardActions, Button } from "@mui/material";
+import { CardContent, CardActions, Button, Tooltip  } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { limitString } from "../services/util";
 import { useHistory } from "react-router-dom";
+import { styled } from '@mui/material/styles';
+import { tooltipClasses } from '@mui/material/Tooltip';
 
 //--estilo--
 const useStyles = makeStyles({
@@ -13,7 +15,16 @@ const useStyles = makeStyles({
   },
   actions: {
     display: "flex",
-    justifyContent: "space-around",
+    marginTop: "auto",
+    justifyContent: "center"
+  },
+});
+
+const BigTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))({
+  [`& .${tooltipClasses.tooltip}`]: {
+    maxWidth: 450,
   },
 });
 //---------
@@ -23,8 +34,8 @@ const ProfCard = ({ info }) => {
   let history = useHistory();
 
   return (
-    <Grid item xs={12} sm={6} md={4} lg={3}>
-        <Card style={{display: "flex", flexDirection: "column", justifyContent: "space-between", height : "350px"}}>
+    <Grid item xs={12} sm={6} md={4} lg={4}>
+        <Card sx={{ display: "flex", flexDirection: "column", maxWidth: 400, height: 450 }}>
           <Box>
             <CardMedia sx={{width: "100%",bgcolor: "#dedede",margin: "auto", backgroundSize: "cover", border: "1px solid #c0c0c0" }}
               className={classes.media} 
@@ -33,13 +44,25 @@ const ProfCard = ({ info }) => {
             />
           </Box>
           
-          <CardContent>
+          <CardContent sx={{width: "100%"}}>
             <Typography variant="subtitle1">{info.nome_exibicao}</Typography>
-            <Typography variant="body1"> {limitString(info.bio, 150)} </Typography>
+
+            <Typography variant="body2" display="inline" style={{textJustify: "justify"}}>
+              {info.bio && limitString(info.bio, 150)} 
+            </Typography>
+
+            { info.bio && info.bio.length > 150 && 
+              <BigTooltip title={info.bio} arrow>            
+                <Typography variant="body2" display="inline" style={{textAlign: "justify", color: "darkblue", fontWeight: 600}}>
+                  {" ..."}
+                </Typography>
+              </BigTooltip>
+            }
           </CardContent>
 
           <CardActions className={classes.actions}>
             <Button
+              variant="outlined"
               color="secondary"
               onClick={() => history.push("/profile", { data: [info.id, info.guid] })}
             >
