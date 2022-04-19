efd075d1ccc569656bef4d6848f9b692a1ecff26
Desnord <desnord12@gmail.com>
2022-04-19 20:28:47 -0300

    fix footer

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -3,9 +3,8 @@ import { Typography, Link } from "@mui/material";
 const Copyright = () => {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/"> Match de Projetos </Link>
+      <Link color="inherit" href="https://mui.com/">Match de Projetos</Link>
       {` ${new Date().getFullYear()}.`} 
     </Typography>
   );
