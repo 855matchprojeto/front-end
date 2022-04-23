951f5ba01eb12fc7ba699c70282805ff11cac00c
Desnord <desnord12@gmail.com>
2022-04-23 11:36:50 -0300

    height fix

--- src/pages/Base.js
+++ src/pages/Base.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from "../components/Header";
 import Copyright from '../components/Copyright';
-import { Box, useMediaQuery } from "@mui/material";
+import { Box } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 //--estilo--
@@ -10,6 +10,7 @@ const useStyles = makeStyles(theme => ({
     container: {
       display: "flex",
       flexDirection: "column",
+      height: "calc(100vh - 64px)",
       paddingLeft: "5%",
       paddingRight: "5%",
       justifyContent: "space-between",
@@ -18,21 +19,13 @@ const useStyles = makeStyles(theme => ({
 //---------
 
 const Base = (props) => {
-    const matches = useMediaQuery('(max-width: 600px)');
-
     const classes = useStyles();
 
     return (
         <>
             <Header />
             <OverlayScrollbarsComponent options={{scrollbars:{autoHide:"scroll"}}}>
-                <div 
-                    className={classes.container}
-                     style={{ 
-                        padding: matches && '0px', 
-                        marginTop: matches ? '0px' :'16px' 
-                    }}
-                  >
+                <div className={classes.container}>
                     {props.children}
                     <Box mt={1} mb={1}>
                       <Copyright/>
