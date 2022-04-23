701e27e3f47774fa4f4899068f4c09775450d910
caiolsds <caiolsds@gmail.com>
2022-04-23 16:35:23 -0300

    Melhoras na página de cadastro

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Link as RouterLink } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
-import {Formik} from 'formik'
+import { Formik } from "formik";
 import * as Yup from "yup";
 import { Cadastrar, Email } from "../services/api_auth";
 
-import { Container, Typography, TextField, Box, createTheme, Alert, Snackbar, Link } from "@mui/material";
-import LoadingButton from '@mui/lab/LoadingButton';
+import {
+  Container,
+  Typography,
+  TextField,
+  Box,
+  createTheme,
+  Link,
+} from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 import Copyright from "../components/Copyright";
 import { makeStyles } from "@mui/styles";
 import { delay } from "../services/util";
@@ -15,7 +23,7 @@ import { delay } from "../services/util";
 //--estilo--
 const theme = createTheme();
 
-const useStyles = makeStyles( ({
+const useStyles = makeStyles({
   paper: {
     marginTop: theme.spacing(8),
     display: "flex",
@@ -34,7 +42,7 @@ const useStyles = makeStyles( ({
     flexDirection: "column",
     marginTop: theme.spacing(1),
   },
-  
+
   textFieldInput: {
     marginTop: theme.spacing(2),
   },
@@ -49,42 +57,40 @@ const useStyles = makeStyles( ({
     alignItems: "center",
     marginTop: theme.spacing(2),
   },
-}));
+});
 //---------
 
 const Cadastro = () => {
+  const { enqueueSnackbar } = useSnackbar();
   const classes = useStyles();
   let history = useHistory();
-
-  const [alert, setAlert] = useState(false);
-  const [severity, setSeverity] = useState('success');
-  const [alertContent, setAlertContent] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
-  function closeAlert(){
-    setAlert(false)
-  }
-
   // initial values / validation / sign up
-  const values = {username: '', nome: '', sobrenome: '', email: '', password: '', password2: ''}
+  const values = {
+    username: "",
+    nome: "",
+    sobrenome: "",
+    email: "",
+    password: "",
+    password2: "",
+  };
 
   const validationSchema = Yup.object().shape({
-    username: Yup.string()
-      .required("campo obrigatório."),
-    nome: Yup.string()
-      .required("campo obrigatório."),
-    sobrenome: Yup.string()
-      .required("campo obrigatório."),
-    email: Yup.string()
-      .email("email inválido")
-      .required("campo obrigatório."),
+    username: Yup.string().required("campo obrigatório."),
+    nome: Yup.string().required("campo obrigatório."),
+    sobrenome: Yup.string().required("campo obrigatório."),
+    email: Yup.string().email("email inválido").required("campo obrigatório."),
     password: Yup.string()
       .min(8, "senha muito curta, deve ter ao minímo 8 caracteres.")
-      .matches("(?=.*[0-9])(?=.*[a-z A-Z])(?=.*[!@#$%^&*])","deve conter ao menos um número, letras e um caracter especial.")
+      .matches(
+        "(?=.*[0-9])(?=.*[a-z A-Z])(?=.*[!@#$%^&*])",
+        "deve conter ao menos um número, letras e um caracter especial."
+      )
       .required("campo obrigatório."),
     password2: Yup.string()
       .required("campo obrigatório.")
-      .oneOf([Yup.ref('password'), null], 'senhas devem ser iguais.')
+      .oneOf([Yup.ref("password"), null], "senhas devem ser iguais."),
   });
 
   async function fazerCadastro(usuario) {
@@ -92,21 +98,25 @@ const Cadastro = () => {
 
     const signup = await Cadastrar(usuario);
 
-    if(signup.status === 200)
-    {
-      setSeverity('success');
-      setAlertContent('Cadastrado com sucesso!');
+    if (signup.status === 200) {
       await Email(signup.data.username);
-      
-      setAlert(true);
-      await delay(5000);
-      history.push('/');
-    }
-    else
-    {
-      setSeverity('error');
-      setAlertContent(signup.response.data.detail);
-      setAlert(true);
+      enqueueSnackbar("Cadastrado realizado com sucesso!", {
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "right",
+        },
+        variant: "success",
+      });
+      delay(1000);
+      history.push("/");
+    } else {
+      enqueueSnackbar(signup.response.data.detail, {
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "right",
+        },
+        variant: "error",
+      });
     }
 
     setIsLoading(false);
@@ -117,93 +127,97 @@ const Cadastro = () => {
   return (
     <Container component="main" maxWidth="xs">
       <div className={classes.paper}>
-
-        <Snackbar open={alert} autoHideDuration={6000} onClose={closeAlert} anchorOrigin={{ vertical:'top', horizontal:'center'}}>
-          <Alert onClose={closeAlert} severity={severity} sx={{ width: '100%' }}>
-            {alertContent}
-          </Alert>
-        </Snackbar>
-        
-        <Typography className={classes.title} align="center" variant="h4">Match de Projetos</Typography>
-        <Typography component="h1" variant="h5" align="center"> Cadastro </Typography>
+        <Typography className={classes.title} align="center" variant="h4">
+          Match de Projetos
+        </Typography>
+        <Typography component="h1" variant="h5" align="center">
+          {" "}
+          Cadastro{" "}
+        </Typography>
 
         <Formik
-          initialValues = {values}
-          validationSchema = {validationSchema}
-          onSubmit = {values => {fazerCadastro(values)}}
+          initialValues={values}
+          validationSchema={validationSchema}
+          onSubmit={(values) => {
+            fazerCadastro(values);
+          }}
         >
-          {props => (
+          {(props) => (
             <form className={classes.form} onSubmit={props.handleSubmit}>
-              <TextField 
-                className={classes.textFieldInput} 
-                id="username" 
-                name="username" 
-                label="nome de usuário" 
-                value={props.values.username} 
-                onChange={props.handleChange} 
+              <TextField
+                className={classes.textFieldInput}
+                id="username"
+                name="username"
+                label="Nome de usuário"
+                value={props.values.username}
+                onChange={props.handleChange}
                 autoComplete="off"
-                error={Boolean(props.touched.username && props.errors.username)} 
+                error={Boolean(props.touched.username && props.errors.username)}
                 helperText={props.errors.username}
               />
 
-              <TextField 
-                className={classes.textFieldInput} 
-                id="nome" 
-                name="nome" 
-                label="nome" 
-                value={props.values.nome} 
-                onChange={props.handleChange} 
+              <TextField
+                className={classes.textFieldInput}
+                id="nome"
+                name="nome"
+                label="Nome"
+                value={props.values.nome}
+                onChange={props.handleChange}
                 autoComplete="off"
-                error={Boolean(props.touched.nome && props.errors.nome)} 
+                error={Boolean(props.touched.nome && props.errors.nome)}
                 helperText={props.errors.nome}
               />
 
-              <TextField 
-                className={classes.textFieldInput} 
+              <TextField
+                className={classes.textFieldInput}
                 id="sobrenome"
-                name="sobrenome" 
-                label="sobrenome" 
-                value={props.values.sobrenome} 
-                onChange={props.handleChange}  
+                name="sobrenome"
+                label="Sobrenome"
+                value={props.values.sobrenome}
+                onChange={props.handleChange}
                 autoComplete="off"
-                error={Boolean(props.touched.sobrenome && props.errors.sobrenome)} 
+                error={Boolean(
+                  props.touched.sobrenome && props.errors.sobrenome
+                )}
                 helperText={props.errors.sobrenome}
               />
 
-              <TextField 
-                className={classes.textFieldInput} 
-                id="email" 
-                name="email" 
-                label="email" 
+              <TextField
+                className={classes.textFieldInput}
+                id="email"
+                name="email"
+                label="Email"
                 error={Boolean(props.touched.email && props.errors.email)}
                 helperText={props.errors.email}
-                value={props.values.email} 
+                value={props.values.email}
                 onChange={props.handleChange}
                 autoComplete="off"
               />
 
-              <TextField 
-                className={classes.textFieldInput} 
-                id="password" 
-                name="password" 
-                label="senha" 
-                type="password" 
+              <TextField
+                className={classes.textFieldInput}
+                id="password"
+                name="password"
+                label="Senha"
+                type="password"
                 error={Boolean(props.touched.password && props.errors.password)}
                 helperText={props.errors.password}
-                value={props.values.password} 
+                value={props.values.password}
                 onChange={props.handleChange}
                 autoComplete="off"
               />
 
-              <TextField 
-                className={classes.textFieldInput} 
-                id="password2" 
-                name="password2" 
-                label="confirmação de senha" 
-                type="password" 
-                error={Boolean(props.touched.password2 && props.errors.password2)}
+              <TextField
+                className={classes.textFieldInput}
+                id="password2"
+                name="password2"
+                label="Confirmação de senha"
+                type="password"
+                error={Boolean(
+                  props.touched.password2 && props.errors.password2
+                )}
                 helperText={props.errors.password2}
-                value={props.values.password2} 
+                value={props.values.password2}
                 onChange={props.handleChange}
                 autoComplete="off"
               />
@@ -213,20 +227,30 @@ const Cadastro = () => {
                 variant="contained"
                 color="primary"
                 className={classes.submit}
-                fullWidth 
+                fullWidth
                 loading={isLoading}
                 endIcon={<></>}
                 loadingPosition="end"
               >
                 Cadastrar
               </LoadingButton>
-
             </form>
           )}
         </Formik>
 
-        <Link className={classes.linkSignin} role='button' component={RouterLink} to="/"> Já tem uma conta? Logar </Link>
-        <Box mt={6} mb={4}> <Copyright /> </Box>
+        <Link
+          className={classes.linkSignin}
+          role="button"
+          component={RouterLink}
+          to="/"
+        >
+          {" "}
+          Já tem uma conta? Logar{" "}
+        </Link>
+        <Box mt={6} mb={4}>
+          {" "}
+          <Copyright />{" "}
+        </Box>
       </div>
     </Container>
   );
