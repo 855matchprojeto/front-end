67861edc081628d0139f6b0d4e0a6988185f34f5
caiolsds <caiolsds@gmail.com>
2022-04-23 22:52:30 -0300

    Foi adicionado o avatar e o menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { NavLink as RouterLink } from "react-router-dom";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Avatar,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  Badge,
+  Button,
+} from "@mui/material";
 import { IconButton, Drawer, Link, createTheme } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import MenuIcon from "@mui/icons-material/Menu";
 import { logout } from "../services/auth";
+import Logout from "@mui/icons-material/Logout";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import axios from "axios";
+import { getToken } from "../services/auth";
 
 //--estilo--
 const theme = createTheme();
@@ -29,6 +43,7 @@ const useStyles = makeStyles({
     flexGrow: 3,
     display: "flex",
     justifyContent: "flex-end",
+    alignItems: "center",
   },
 
   navLink: {
@@ -88,8 +103,27 @@ const useStyles = makeStyles({
 const Header = () => {
   const classes = useStyles();
   const [view, setView] = useState({ mobileView: false, drawerOpen: false });
+  const [notifications, setNotifications] = useState([]);
+
+  const getNotifications = async () => {
+    const res = await axios.get(
+      "https://notification-match-projetos.herokuapp.com/users/user/me/get-notifications?is_read=false",
+      {
+        headers: {
+          Authorization: `Bearer ${getToken}`,
+        },
+      }
+    );
+
+    if (res.status === 200) {
+      setNotifications(res.data);
+    }
+    console.log(res);
+  };
 
   useEffect(() => {
+    getNotifications();
+
     const setResponsiveView = () => {
       if (window.innerWidth < 900) {
         setView((previous) => ({ ...previous, mobileView: true }));
@@ -107,6 +141,15 @@ const Header = () => {
   }, []);
 
   const DisplayDesktop = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const avatarMenu = useRef();
+    const handleOpen = () => {
+      setAnchorEl(avatarMenu.current);
+    };
+
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
     return (
       <Toolbar className={classes.toolbar}>
         <Link
@@ -136,22 +179,79 @@ const Header = () => {
             Projetos
           </Link>
           <Link
-            component={RouterLink}
-            to="/perfil"
-            className={classes.navLink}
-            activeClassName={classes.activeNav}
+            ref={avatarMenu}
+            component={Button}
+            onClick={() => handleOpen()}
+            sx={{
+              ml: 5,
+            }}
           >
-            {" "}
-            Perfil{" "}
+            <Badge
+              color="error"
+              badgeContent={notifications.length}
+              sx={{ cursor: "pointer" }}
+            >
+              <Avatar />
+            </Badge>
           </Link>
-          <Link
-            role="Button"
-            onClick={() => logout()}
-            className={classes.navLink}
+          <Menu
+            open={Boolean(anchorEl)}
+            anchorEl={anchorEl}
+            onClose={() => handleClose()}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+            PaperProps={{
+              sx: {
+                overflow: "visible",
+                py: 0.7,
+                px: 1,
+                "& .MuiAvatar-root": {
+                  width: 32,
+                  height: 32,
+                  ml: -0.5,
+                  mr: 1,
+                },
+                "&:before": {
+                  content: '""',
+                  display: "block",
+                  position: "absolute",
+                  top: 0,
+                  right: 20,
+                  width: 10,
+                  height: 10,
+                  bgcolor: "background.paper",
+                  transform: "translateY(-50%) rotate(45deg)",
+                  zIndex: 0,
+                },
+              },
+            }}
           >
-            {" "}
-            Sair{" "}
-          </Link>
+            <MenuItem component={RouterLink} to="/perfil" sx={{ mb: 1 }}>
+              <Avatar /> Perfil
+            </MenuItem>
+            <MenuItem component={RouterLink} to="/perfil" sx={{ mb: 1 }}>
+              <ListItemIcon>
+                <Badge
+                  color="error"
+                  variant="dot"
+                  badgeContent={notifications.length}
+                >
+                  <NotificationsIcon fontSize="small" />
+                </Badge>
+              </ListItemIcon>
+              Notificações
+            </MenuItem>
+            <MenuItem
+              component="button"
+              onClick={() => logout()}
+              sx={{ width: "100%" }}
+            >
+              <ListItemIcon>
+                <Logout fontSize="small" />
+              </ListItemIcon>
+              Sair
+            </MenuItem>
+          </Menu>
         </nav>
       </Toolbar>
     );
