cb804f823a3f3a90426b5e97e9c995e4c7e2b993
Desnord <desnord12@gmail.com>
2022-05-09 16:48:00 -0300

    teste lazy load

--- src/components/dialogs/AlertDialog.jsx
+++ src/components/dialogs/AlertDialog.jsx
@@ -1,7 +1,11 @@
-import React, { useState, forwardRef } from "react";
+import React, { Suspense, useState, forwardRef } from "react";
 import { IconButton } from "@mui/material";
-import {Dialog, DialogContent, DialogContentText, DialogTitle, Slide} from "@mui/material";
+import {Dialog, Slide} from "@mui/material";
 import InfoIcon from '@mui/icons-material/Info';
+
+const DialogTitle = React.lazy(() => import('@mui/material/DialogTitle'));
+const DialogContent = React.lazy(() => import('@mui/material/DialogContent'));
+const DialogContentText = React.lazy(() => import('@mui/material/DialogContentText'));
 
 const Transition = forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
@@ -25,24 +29,26 @@ function AlertDialog(props)
         onClose={() => setOpen(false)}
         aria-describedby="dialog-desc"
       >
-        <DialogTitle>{"Como pesquisar?"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="dialog-desc">
-            { type ?
-              <> 
-                Busque por usuários da plataforma. Caso possuir algum projeto,
-                você pode selecionar algum específico, e buscar por usuários que se encaixam em algum perfil.
-                caso encontre algum que se encaixe, pode marcá-lo como de interesse do projeto.
-              </> 
-              :
-              <> 
-                Busque por projetos da plataforma. 
-                Caso encontre algum que goste, pode marcá-lo como de seu interesse.
-              </>
-            }
-            
-          </DialogContentText>
-        </DialogContent>
+        <Suspense fallback={<></>}>
+            <DialogTitle>{"Como pesquisar?"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="dialog-desc">
+                { type ?
+                  <> 
+                    Busque por usuários da plataforma. Caso possuir algum projeto,
+                    você pode selecionar algum específico, e buscar por usuários que se encaixam em algum perfil.
+                    caso encontre algum que se encaixe, pode marcá-lo como de interesse do projeto.
+                  </> 
+                  :
+                  <> 
+                    Busque por projetos da plataforma. 
+                    Caso encontre algum que goste, pode marcá-lo como de seu interesse.
+                  </>
+                }
+                
+              </DialogContentText>
+            </DialogContent>
+        </Suspense>
       </Dialog>
     </>
   );
